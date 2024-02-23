0ff1fe952d11fbf7ab9e894e58d76f39afd65545
rezamazzz <rezamaz127@gmail.com>
2024-02-23 12:32:16 +0330

    Add Footer and Home Page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import './App.css'
-import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
-import React from 'react'
-import Home from './pages/Home'
-import Projects from './pages/Projects'
-import Experience from './pages/Experience'
-import Navbar from './components/Navbar.js';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React from "react";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import Experience from "./pages/Experience";
+import Navbar from "./components/Navbar.js";
+import Footer from "./components/Footer.js";
 const App = () => {
-  
   return (
-    <div className='App'>
+    <div className="App">
       <Router>
-        <Navbar/>
+        <Navbar />
         <Routes>
-          <Route path='/' element={<Home/>}/>
-          <Route path='/projects' element={<Projects/>}/>
-          <Route path='/experience' element={<Experience/>}/>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/experience" element={<Experience />} />
         </Routes>
+        <Footer />
       </Router>
-    
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,46 @@
-import React from 'react'
+import React from "react";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import EmailIcon from "@mui/icons-material/Email";
+import GithubIcon from "@mui/icons-material/GitHub";
+import "../styles/home.css";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className="home">
+      <div className="about">
+        <h2>Hello My Name Is Reza </h2>
+        <div className="prompt">
+        <p>Software Developer With Passion For Learning and Creating</p>
+        <LinkedInIcon />
+        <EmailIcon />
+        <GithubIcon />
+        </div>
+      </div>
+      <div className="skills">
+        <h1>Skills</h1>
+        <ol className="list">
+          <li className="item">
+            <h2>Front-End</h2>
+            <span>
+              ReactJs,Redux,Angular,HTML,CSS,React Native,Flutter,NPM,
+              Ionic,BootStrap,MaterialUI,Yarn,TailwindCSS,StyleComponnents
+            </span>
+          </li>
+          <li className="item">
+            <h2>Back-End</h2>
+            <span>
+              Node.Js, JavaSpring,.Net , ExpressJs,GraphQL,ApolloServer, Mysql,
+              MongoDB ,DynamoDB, AWS S3,MS Sql
+            </span>
+          </li>
+          <li className="item">
+            <h2>Languges</h2>
+            <span>JavaScript,Java,Python,C,C#,C++,Typescript,Go</span>
+          </li>
+        </ol>
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
